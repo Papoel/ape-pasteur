616b15d9dae557ab1dfd13ece6a2a2346b73c25b
Papoel <pascal.briffard@gmail.com>
2022-02-17 00:27:46 +0100

    Création de la HomePage (WIP)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[IsGranted('ROLE_USER')]
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/home', name: 'app_home')]
+    public function home(EntityManagerInterface $em): Response
+    {
+        $articles = $em->getRepository(Article::class)->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+}
