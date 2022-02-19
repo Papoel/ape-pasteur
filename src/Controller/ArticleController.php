df456b2ccde9b1a5bcd61369b9f3cb309a8cbed5
Papoel <pascal.briffard@gmail.com>
2022-02-19 17:09:46 +0100

    Création de la page Article - Activation du lien dans la navbar

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/article')]
+class ArticleController extends AbstractController
+{
+    #[Route('/', name: 'article_index', methods: ['GET'])]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findBy([], ['createdAt' => 'DESC']),
+        ]);
+    }
+}
