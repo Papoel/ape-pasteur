eb1abc13bf724687ae0f0c7715e1ca6fb948f8cc
Papoel <pascal.briffard@gmail.com>
2022-02-19 17:49:53 +0100

    Ajout de la fonction __ToString pour retourner Prénom + Nom de l'auteur

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -77,6 +77,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->articles = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->firstname.' '.$this->lastname;
+    }
+
     public function getFullName(): string
     {
         $firstname = $this->firstname;
