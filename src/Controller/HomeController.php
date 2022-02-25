ebffaba587d66885e2d4e2ce06069fd6c6faedb6
Papoel <pascal.briffard@gmail.com>
2022-02-25 20:53:31 +0100

    Protection de la route Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,28 +7,45 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class HomeController extends AbstractController
 {
+    public function __construct(private Security $security)
+    {
+    }
+
     #[Route('/', name: 'index')]
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
+        $user = $this->security->getUser();
 
-        return $this->redirectToRoute('app_home');
+        if (false === $user->getIsValide()) {
+            $this->redirectToRoute('app_error_user_not_valid');
+        } else {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->redirectToRoute('app_error_user_not_valid');
     }
 
     #[Route('/home', name: 'app_home')]
     public function home(EntityManagerInterface $em): Response
     {
+        $user = $this->security->getUser();
         $this->denyAccessUnlessGranted('ROLE_USER');
 
         $articles = $em->getRepository(Article::class)->findAll();
 
-        // Todo : Si isValide = false return to error/user-is_not_valide.html.twig
+        if (false === $user->getIsValide()) {
+            $this->redirectToRoute('app_error_user_not_valid');
+        } else {
+            return $this->render('home/index.html.twig', [
+                'articles' => $articles,
+            ]);
+        }
 
-        return $this->render('home/index.html.twig', [
-            'articles' => $articles,
-        ]);
+        return $this->redirectToRoute('app_error_user_not_valid');
     }
 }
