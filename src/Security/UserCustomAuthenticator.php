1dde2d22c07c7ed32c86bfe589aa882b073a2b64
Papoel <pascal.briffard@gmail.com>
2022-03-09 20:50:43 +0100

    Fix: Oubli de 'generate'

--- src/Security/UserCustomAuthenticator.php
+++ src/Security/UserCustomAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -45,6 +46,7 @@ class UserCustomAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        /** @var User $user */
         $user = $token->getUser();
 
         /* TODO: Remplacer par la route admin */
@@ -73,7 +75,7 @@ class UserCustomAuthenticator extends AbstractLoginFormAuthenticator
         //return new RedirectResponse($this->urlGenerator->generate('some_route'));
 //        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
 //        return new RedirectResponse($this->urlGenerator->generate('app_home'));
-        return new RedirectResponse($this->urlGenerator('app_home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl(Request $request): string
